d7d873db2e16554ceb7f78feed30bfe5634e0458
Derek <dere@dhigroup.com>
2026-01-22 11:51:49 -0500

    added missing pygments requirement

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "dask==2024.2.1",
         "matplotlib",
         "PyQt5",
+        "pygments>=2.13.0,<3.0.0",
         "GDAL @ https://github.com/derekeden/trackio/releases/download/v0.2.0/GDAL-3.8.4-cp310-cp310-win_amd64.whl",
         "rasterio @ https://github.com/derekeden/trackio/releases/download/v0.2.0/rasterio-1.3.9-cp310-cp310-win_amd64.whl",
         "inpoly @ https://github.com/derekeden/trackio/releases/download/v0.2.0/inpoly-0.2.0-cp310-cp310-win_amd64.whl",
